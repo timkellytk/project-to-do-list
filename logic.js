5d65da640a79ef5dc3a4f2d1da706caa0f15ee2a
Tim Kelly <timpkelly1@gmail.com>
2020-04-08 16:00:25 +1000

    Adding in extra properties for the to-do-list items

--- logic.js
+++ logic.js
@@ -13,18 +13,22 @@ function resetProjectsInactive() {
         obj.active = false;
     })
 }
-const toDoItem = (title, description) => {
+const toDoItem = (title, description, dueDate, priority, notes) => {
     let activeProject = allProjects.filter(project => project.active == true)
     activeProject[0].toDoItems.push({
         title, 
         description,
+        dueDate,
+        priority,
+        notes,
+        complete: false,
     })
 }
 
 // Declaring test variables
 project('default1')
-toDoItem('Complete my washing', 'Get that good job ye')
+toDoItem('Complete my washing', 'Get that good job ye', '1st June 2019', 'High', 'Notes...')
 project('default2')
-toDoItem('Complete my exercise', 'Get that good job ye')
+toDoItem('Complete my exercise', 'Get that good job ye', '1st June 2019', 'High', 'Notes...')
 project('default3')
-toDoItem('Complete The Odin Project', 'Get that good job ye')
+toDoItem('Complete The Odin Project', 'Get that good job ye', '1st June 2019', 'High', 'Notes...')
