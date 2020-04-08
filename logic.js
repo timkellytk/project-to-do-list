65e730e42c5e2bbaad6adebc285f5c220be2f6de
Tim Kelly <timpkelly1@gmail.com>
2020-04-08 17:17:59 +1000

    Cleaning up the select active project, todoarray and todoitems and cleaning up the code structure

--- logic.js
+++ logic.js
@@ -1,29 +1,90 @@
-// Project logic
+// Factory functions
 const allProjects = [];
 const createProject = (name) => {
-    resetAllProjectsInactive();
+    editAllProjectsInactive();
     allProjects.push({
         name,
         toDoItems: [], 
         active: true,
     });
 }
-function resetAllProjectsInactive() {
-    allProjects.forEach((obj) => {
-        obj.active = false;
-    })
-}
-const createToDo = (title, description, dueDate, priority, notes) => {
-    let activeProject = allProjects.filter(project => project.active === true)
-    let toDoArray = activeProject[0].toDoItems
+const createToDo = (name, description, dueDate, priority, notes) => {
+    let activeProject = allProjects.filter(project => project.active === true);
+    let toDoArray = activeProject[0].toDoItems;
     toDoArray.push({
-        title, 
+        name, 
         description,
         dueDate,
         priority,
         notes,
+        active: true,
         complete: false,
-    })
+    });
+}
+
+// Select functions
+function selectActiveProject() {
+    let activeProjectArray = allProjects.filter(project => project.active === true)
+    return activeProjectArray[0];
+}
+function selectActiveToDoArray() {
+    let activeProject = allProjects.filter(project => project.active === true);
+    let activeToDoArray = activeProject[0].toDoItems;
+    return activeToDoArray[0];
+}
+function selectActiveToDo(){
+    let activeProject = allProjects.filter(project => project.active === true);
+    let activeToDoArray = activeProject[0].toDoItems;
+    let activeToDo = toDoArray.filter(toDo => toDo.active === true);
+    return activeToDo[0];
+}
+
+// Active functions
+
+
+
+// Project functions
+function deleteProject() {
+    let activeProject = selectActiveProject();
+    let index = allProjects.indexOf(activeProject);
+    allProjects.splice(index, 1)
+}
+function editProjectName(name) {
+    let activeProject = selectActiveProject();
+    activeProject.name = name;
+}
+function editAllProjectsInactive() {
+    allProjects.forEach((obj) => {
+        obj.active = false;
+    });
+}
+
+// To do functions
+function deleteToDo() {
+    let toDoArray = selectActiveToDoArray();
+    let toDo = selectActiveToDo();
+    let index = toDoArray.indexOf(toDo)
+    toDoArray.splice(index, 1)
+}
+function editToDoName(name) {
+    let toDo = selectActiveToDo();
+    toDo[0].name = name;
+}
+function editToDoDescription(description) {
+    let toDo = selectActiveToDo();
+    toDo[0].description = description;
+}
+function editToDoDueDate(dueDate) {
+    let toDo = selectActiveToDo();
+    toDo[0].dueDate = dueDate;
+}
+function editToDoPriority(priority) {
+    let toDo = selectActiveToDo();
+    toDo[0].priority = priority;
+}
+function editToDoNotes(notes) {
+    let toDo = selectActiveToDo();
+    toDo[0].notes = notes;
 }
 
 // Test variables
