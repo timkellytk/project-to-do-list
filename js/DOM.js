7de4da0b01e1722e0f5cd42a7764407d41bb6e68
Tim Kelly <timpkelly1@gmail.com>
2020-04-09 17:14:02 +1000

    Created the DOM layer JS code for the newTask items

--- js/DOM.js
+++ js/DOM.js
@@ -1,34 +1,38 @@
 // New project form
 const newProject = (() => {
-    const _div = document.querySelector('.new-project-block');
+    const addBtn = document.querySelector('#add-project');
+    const cancelBtn = document.querySelector('.cancel-btn.project-create');
+    const submitBtn = document.querySelector('.submit-btn.project-create');
+    const _form = document.querySelector('.new-project-block');
     const _name = document.querySelector('[name = new-project-name]');
     const _description = document.querySelector('[name = new-project-description]');
+
     function show() {
-        _div.style.display = 'flex';
+        _form.style.display = 'flex';
     };
     function hide() {
-        _div.style.display = 'none';
-    };
-    function _submit() {
-        console.log(_name.value);
-        console.log(_description.value);
+        _form.style.display = 'none';
+        _clear();
     };
     function _clear() {
         _name.value = '';
         _description.value = '';
     };
+    function _submit() {
+        console.log(_name.value);
+        console.log(_description.value);
+    };
+
     function create() {
         if (_name.value !== '') {
             _submit();
             _clear();
             hide();
         } else {
-            alert('You need to enter a name for your project');
+            alert('Please add a name for your project');
         }
     };
-    const addBtn = document.querySelector('#add-project');
-    const cancelBtn = document.querySelector('.cancel-btn.project-create');
-    const submitBtn = document.querySelector('.submit-btn.project-create');
+
     return {
       show,
       hide,
@@ -43,6 +47,61 @@ newProject.addBtn.addEventListener('click', newProject.show);
 newProject.cancelBtn.addEventListener('click', newProject.hide);
 newProject.submitBtn.addEventListener('click', newProject.create);
 
+// New task form
 const newTask = (() => {
+    const addBtn = document.querySelector('#add-task')
+    const cancelBtn = document.querySelector('.cancel-btn.task-create')
+    const submitBtn = document.querySelector('.submit-btn.task-create')
+    const _form = document.querySelector('.create-task')
+    const _task = document.querySelector('[name = new-task]');
 
+
+    function _showForm() {
+        _form.style.display = 'block' 
+    };
+    function _hideForm() {
+        _form.style.display = 'none' 
+    }
+    function _showAddBtn() {
+        addBtn.style.display = 'block'
+    }
+    function _hideAddBtn() {
+        addBtn.style.display = 'none'
+    }
+    function _clearTask() {
+        _task.value = '';
+    }
+
+    function show() {
+        _showForm();
+        _hideAddBtn();
+    }
+    function hide() {
+        _showAddBtn();
+        _hideForm();
+        _clearTask();
+    }
+
+    function create() {
+        if(_task.value !== '') {
+            console.log(_task.value)
+            _clearTask();
+            hide();
+        } else {
+            alert('Please enter a name for your task')
+        }
+    }
+
+    return {
+        show,
+        hide,
+        create,
+        addBtn,
+        cancelBtn,
+        submitBtn,
+    };
 })();
+
+newTask.addBtn.addEventListener('click', newTask.show)
+newTask.cancelBtn.addEventListener('click', newTask.hide)
+newTask.submitBtn.addEventListener('click', newTask.create)
