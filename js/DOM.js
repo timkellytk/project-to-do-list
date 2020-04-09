f79627870a755b8bdb14825272e1a0f357a27059
Tim Kelly <timpkelly1@gmail.com>
2020-04-09 16:35:26 +1000

    Fixed the DOM layer JS code for newProject items

--- js/DOM.js
+++ js/DOM.js
@@ -1,6 +1,8 @@
 // New project form
 const newProject = (() => {
-    let _div = document.querySelector('.new-project-block');
+    const _div = document.querySelector('.new-project-block');
+    const _name = document.querySelector('[name = new-project-name]');
+    const _description = document.querySelector('[name = new-project-description]');
     function show() {
         _div.style.display = 'flex';
     };
@@ -8,18 +10,25 @@ const newProject = (() => {
         _div.style.display = 'none';
     };
     function _submit() {
-        let name = document.querySelector('[name = new-project-name]');
-        let description = document.querySelector('[name = new-project-description]');
-        console.log(name.value);
-        console.log(description.value);
+        console.log(_name.value);
+        console.log(_description.value);
+    };
+    function _clear() {
+        _name.value = '';
+        _description.value = '';
     };
     function create() {
-        _submit();
-        hide();
+        if (_name.value !== '') {
+            _submit();
+            _clear();
+            hide();
+        } else {
+            alert('You need to enter a name for your project');
+        }
     };
-    let addBtn = document.querySelector('.add-project');
-    let cancelBtn = document.querySelector('.cancel-btn.project-create');
-    let submitBtn = document.querySelector('.submit-btn.project-create');
+    const addBtn = document.querySelector('#add-project');
+    const cancelBtn = document.querySelector('.cancel-btn.project-create');
+    const submitBtn = document.querySelector('.submit-btn.project-create');
     return {
       show,
       hide,
@@ -33,3 +42,7 @@ const newProject = (() => {
 newProject.addBtn.addEventListener('click', newProject.show);
 newProject.cancelBtn.addEventListener('click', newProject.hide);
 newProject.submitBtn.addEventListener('click', newProject.create);
+
+const newTask = (() => {
+
+})();
