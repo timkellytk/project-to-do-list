fe9ebbe5db0161278d588bd12d992cc362e9098c
Tim Kelly <timpkelly1@gmail.com>
2020-04-10 09:33:48 +1000

    Created a function to add all of the active tasks to the DOM layer and created a function that loads tasks

--- js/DOM.js
+++ js/DOM.js
@@ -43,6 +43,103 @@ function loadActiveProjectInfo() {
 
 loadActiveProjectInfo();
 
+function loadTasks(task) {
+    const contentContainer = document.querySelector('#all-tasks')
+
+        const toDo = document.createElement('div')
+        toDo.classList.add('to-do')
+
+            const toDoContainer = document.createElement('div')
+            toDoContainer.classList.add('task-container')
+            
+                const left = document.createElement('div')
+                left.classList.add('horizontal-flex')
+
+                    const checkbox = document.createElement('div')
+                    checkbox.classList.add('checkbox')
+
+                    const name = document.createElement('div')
+                    name.textContent = task.name
+                    name.classList.add('task-name')
+
+                left.appendChild(checkbox)
+                left.appendChild(name)
+
+                const right = document.createElement('div')
+                right.classList.add('horizontal-flex')
+
+                    const date = document.createElement('div')
+                    date.classList.add('date-text')
+                    date.textContent = task.dueDate
+
+                    const editIcon = document.createElement('img')
+                    editIcon.classList.add('utility-btn')
+                    editIcon.classList.add('edit-task')
+                    editIcon.src = 'images/pen.svg'
+
+                    const deleteIcon = document.createElement('img')
+                    deleteIcon.classList.add('utility-btn')
+                    deleteIcon.classList.add('delte-task')
+                    deleteIcon.src = 'images/icons8-trash-1.svg'
+
+                right.appendChild(date)
+                right.appendChild(editIcon)
+                right.appendChild(deleteIcon)
+
+            toDoContainer.appendChild(left)
+            toDoContainer.appendChild(right)
+
+            const toDoForm = document.createElement('div')
+            toDoForm.classList.add('w-form')
+            toDoForm.classList.add('edit-task-form')
+
+                const form = document.createElement('form')
+                form.classList.add('flex-form')
+
+                    const inputName = document.createElement('input')
+                    inputName.classList.add('w-input')
+                    inputName.classList.add('edit-task-name')
+
+                    const containerBtns = document.createElement('div')
+                    containerBtns.classList.add('horizontal-flex')
+
+                        const submitBtn = document.createElement('button')
+                        submitBtn.classList.add('w-button')
+                        submitBtn.classList.add('submit-btn')
+                        submitBtn.classList.add('edit-task')
+                        submitBtn.textContent = "Submit"
+                        submitBtn.min
+
+                        const cancelBtn = document.createElement('div')
+                        cancelBtn.classList.add('cancel-btn')
+                        cancelBtn.classList.add('task-edit')
+                        cancelBtn.textContent = "Cancel"
+
+                    containerBtns.appendChild(submitBtn)
+                    containerBtns.appendChild(cancelBtn)
+
+                form.appendChild(inputName)
+                form.appendChild(containerBtns)
+
+            toDoForm.appendChild(form)
+
+        toDo.appendChild(toDoContainer)
+        toDo.appendChild(toDoForm)
+    contentContainer.appendChild(toDo)
+}
+
+function loadActiveTasks() {
+    allProjects.forEach((project) => {
+        project.toDoItems.forEach((task) => {
+            if (project.active === true){
+                loadTasks(task);
+            }
+        })
+    })
+}
+
+loadActiveTasks();
+
 
 // New project form
 const newProject = (() => {
