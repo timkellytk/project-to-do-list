adaab2a5c9fd4f2357edac84498c1a54fdd5245b
Tim Kelly <timpkelly1@gmail.com>
2020-04-10 09:50:10 +1000

    Cleaned up the JS code by splitting the DOM.js file into loadDOM.js and functionalityDOM.JS and deleting the webflow.js file - used for nothing

---
+++ js/functionalityDOM.js
@@ -0,0 +1,191 @@
+// New project form
+const newProject = (() => {
+    const addBtn = document.querySelector('#add-project');
+    const cancelBtn = document.querySelector('.cancel-btn.project-create');
+    const submitBtn = document.querySelector('.submit-btn.project-create');
+    const _form = document.querySelector('.new-project-block');
+    const _name = document.querySelector('[name = new-project-name]');
+    const _description = document.querySelector('[name = new-project-description]');
+
+    function show() {
+        _form.style.display = 'flex';
+    };
+    function hide() {
+        _form.style.display = 'none';
+        _clear();
+    };
+    function _clear() {
+        _name.value = '';
+        _description.value = '';
+    };
+    function _submit() {
+        console.log(_name.value);
+        console.log(_description.value);
+    };
+
+    function create() {
+        if (_name.value !== '') {
+            _submit();
+            _clear();
+            hide();
+        } else {
+            alert('Please add a name for your project');
+        }
+    };
+
+    return {
+      show,
+      hide,
+      create,
+      addBtn,
+      cancelBtn,
+      submitBtn,
+    };
+})();
+
+newProject.addBtn.addEventListener('click', newProject.show);
+newProject.cancelBtn.addEventListener('click', newProject.hide);
+newProject.submitBtn.addEventListener('click', newProject.create);
+
+// Edit project form
+
+const editProject = (() => {
+    const editBtn = document.querySelector('.edit-project-info');
+    const cancelBtn = document.querySelector('.cancel-btn.edit-project')
+    const submitBtn = document.querySelector('.submit-btn.edit-project')
+    const _content = document.querySelector('.project-info-display');
+    const _name = document.querySelector('[name = project-name]');
+    const _description = document.querySelector('[name = project-description]');
+    const _form = document.querySelector('.edit-project-form');
+
+    function _showForm() {
+        _form.style.display = 'block';
+    };
+    function _hideForm() {
+        _form.style.display = 'none';
+    };
+    function _showContent() {
+        _content.style.display = 'block'
+    }
+    function _hideContent() {
+        _content.style.display = 'none';
+    };
+
+    function show() {
+        _showForm();
+        _hideContent();
+    };
+    function hide() {
+        _showContent();
+        _hideForm();
+    }
+    function _clear() {
+        _name.value = '';
+        _description.value = '';
+    }
+    function submit() {
+        console.log(_name.value);
+        console.log(_description.value);
+        hide();
+        _clear();
+    }
+    
+    return {
+        editBtn,
+        cancelBtn,
+        submitBtn,
+        show,
+        hide,
+        submit,
+    }
+})();
+
+editProject.editBtn.addEventListener('click', editProject.show)
+editProject.cancelBtn.addEventListener('click', editProject.hide)
+editProject.submitBtn.addEventListener('click', editProject.submit)
+
+// New task form
+const newTask = (() => {
+    const addBtn = document.querySelector('#add-task')
+    const cancelBtn = document.querySelector('.cancel-btn.task-create')
+    const submitBtn = document.querySelector('.submit-btn.task-create')
+    const _form = document.querySelector('.create-task')
+    const _task = document.querySelector('[name = new-task]');
+
+
+    function _showForm() {
+        _form.style.display = 'block' 
+    };
+    function _hideForm() {
+        _form.style.display = 'none' 
+    }
+    function _showAddBtn() {
+        addBtn.style.display = 'block'
+    }
+    function _hideAddBtn() {
+        addBtn.style.display = 'none'
+    }
+    function _clearTask() {
+        _task.value = '';
+    }
+
+    function show() {
+        _showForm();
+        _hideAddBtn();
+    }
+    function hide() {
+        _showAddBtn();
+        _hideForm();
+        _clearTask();
+    }
+
+    function create() {
+        if(_task.value !== '') {
+            console.log(_task.value)
+            _clearTask();
+            hide();
+        } else {
+            alert('Please enter a name for your task')
+        }
+    }
+
+    return {
+        show,
+        hide,
+        create,
+        addBtn,
+        cancelBtn,
+        submitBtn,
+    };
+})();
+
+newTask.addBtn.addEventListener('click', newTask.show)
+newTask.cancelBtn.addEventListener('click', newTask.hide)
+newTask.submitBtn.addEventListener('click', newTask.create)
+
+// Edit task form
+const editTask = (() => {
+    btns = document.querySelectorAll('.utility-btn.edit-task');
+
+    function show() {
+        console.log('the show works for editTask')
+    }
+    /* 
+    I realised I need a data-value for each task in the to-do-list in the array.
+    This way I can reference the appropriate form and such
+    */
+    return {
+        btns,
+        show,
+    }
+})();
+
+editTask.btns.forEach((btn) => {
+    btn.addEventListener('click', editTask.show)
+});
+
+// Mark task as complete
+
+
+ // Delete task
+
---
+++ js/loadDOM.js
@@ -0,0 +1,146 @@
+// Loading projects into DOM layer
+
+function loadProjects() {
+    allProjects.forEach((project) => {
+        const container = document.querySelector('#all-projects')
+        const div = document.createElement('div')
+        div.classList.add('project-text')
+        div.classList.add('project-container')
+        if (project.active === true) {
+            div.classList.add('active')
+        }
+
+        const text = document.createElement('div')
+        text.textContent = project.name
+
+        const image = document.createElement('img')
+        image.src = 'images/icons8-trash-1.svg'
+        image.classList.add('utility-btn')
+
+        div.appendChild(text)
+        div.appendChild(image)
+        container.appendChild(div)
+    })
+}
+
+loadProjects();
+
+function loadActiveProjectInfo() {
+    let activeProject = allProjects.filter((project) => project.active === true)
+    let headingValue = activeProject[0].name
+    let headingDescription = activeProject[0].description
+
+    const heading = document.querySelector('#project-name');
+    const inputHeading = document.querySelector('[name=edit-project-name]')
+    const description = document.querySelector('#project-description')
+    const inputDescripton = document.querySelector('[name=edit-project-description')
+    
+    heading.textContent = headingValue
+    inputHeading.value = headingValue
+    description.textContent = headingDescription
+    inputDescripton.value = headingDescription
+}
+
+loadActiveProjectInfo();
+
+function loadTasks(task) {
+    const contentContainer = document.querySelector('#all-tasks')
+
+        const toDo = document.createElement('div')
+        toDo.classList.add('to-do')
+
+            const toDoContainer = document.createElement('div')
+            toDoContainer.classList.add('task-container')
+            
+                const left = document.createElement('div')
+                left.classList.add('horizontal-flex')
+
+                    const checkbox = document.createElement('div')
+                    checkbox.classList.add('checkbox')
+
+                    const name = document.createElement('div')
+                    name.textContent = task.name
+                    name.classList.add('task-name')
+
+                left.appendChild(checkbox)
+                left.appendChild(name)
+
+                const right = document.createElement('div')
+                right.classList.add('horizontal-flex')
+
+                    const date = document.createElement('div')
+                    date.classList.add('date-text')
+                    date.textContent = task.dueDate
+
+                    const editIcon = document.createElement('img')
+                    editIcon.classList.add('utility-btn')
+                    editIcon.classList.add('edit-task')
+                    editIcon.src = 'images/pen.svg'
+
+                    const deleteIcon = document.createElement('img')
+                    deleteIcon.classList.add('utility-btn')
+                    deleteIcon.classList.add('delte-task')
+                    deleteIcon.src = 'images/icons8-trash-1.svg'
+
+                right.appendChild(date)
+                right.appendChild(editIcon)
+                right.appendChild(deleteIcon)
+
+            toDoContainer.appendChild(left)
+            toDoContainer.appendChild(right)
+
+            const toDoForm = document.createElement('div')
+            toDoForm.classList.add('w-form')
+            toDoForm.classList.add('edit-task-form')
+
+                const form = document.createElement('form')
+                form.classList.add('flex-form')
+
+                    const inputName = document.createElement('input')
+                    inputName.classList.add('w-input')
+                    inputName.classList.add('edit-task-name')
+
+                    const containerBtns = document.createElement('div')
+                    containerBtns.classList.add('horizontal-flex')
+
+                        const submitBtn = document.createElement('button')
+                        submitBtn.classList.add('w-button')
+                        submitBtn.classList.add('submit-btn')
+                        submitBtn.classList.add('edit-task')
+                        submitBtn.textContent = "Submit"
+                        submitBtn.min
+
+                        const cancelBtn = document.createElement('div')
+                        cancelBtn.classList.add('cancel-btn')
+                        cancelBtn.classList.add('task-edit')
+                        cancelBtn.textContent = "Cancel"
+
+                    containerBtns.appendChild(submitBtn)
+                    containerBtns.appendChild(cancelBtn)
+
+                form.appendChild(inputName)
+                form.appendChild(containerBtns)
+
+            toDoForm.appendChild(form)
+
+        toDo.appendChild(toDoContainer)
+        toDo.appendChild(toDoForm)
+    contentContainer.appendChild(toDo)
+}
+
+function addTaskEventListeners(editIcon, deleteIcon) {
+    editIcon.addEventListener('click', () => console.log('edit the thing'))
+    deleteIcon.addEventListener('click', () => console.log('delete the thing'))
+}
+
+function loadActiveTasks() {
+    allProjects.forEach((project) => {
+        project.toDoItems.forEach((task) => {
+            if (project.active === true){
+                loadTasks(task);
+            }
+        })
+    })
+}
+
+loadActiveTasks();
